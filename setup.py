a55ec203a7611d20a33ac5153bc2d451034f28bc
mariondss <marion.de_sousa@edu.devinci.fr>
2024-01-28 17:58:32 +0100

    ajout setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='Periodic Table',
+ version='1.0',
+ author='mariondss',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
+
