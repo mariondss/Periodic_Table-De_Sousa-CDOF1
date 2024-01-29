7f1eb5e5564c48f583d9646edbae0a08c6867b39
Sbike <leroyhubert@yahoo.fr>
2024-01-29 19:45:36 +0100

    Changed the answering way (Now the input is a number) And a feature to check if the input is a valid number

--- Periodic_Table.py
+++ Periodic_Table.py
@@ -1,55 +1,86 @@
+from enum import Enum
+
+class Score(Enum):
+    LOSER = 0
+    NOT_BEST = range(1, 4)
+    ALMOST_PERFECT = 4
+    PERFECT = 5
+
 class Question:
+    """This class represents a question in the quiz."""
     def __init__(self, prompt, options, answer):
         self.prompt = prompt
         self.options = options
-        self.answer = answer.lower()
+        self.answer = answer
 
     def display(self):
+        """This method displays the question and its options."""
         result = self.prompt + "\n"
-        for option in self.options:
-            result += "- " + option + "\n"
+        for i, option in enumerate(self.options, start=1):
+            result += f"{i}. {option}\n"
         return result
 
     def check_answer(self, user_answer):
-        return self.answer == user_answer.lower()
-
+        """This method checks if the user's answer is correct."""
+        return self.answer == self.options[int(user_answer) - 1]
 
 class Quiz:
+    """This class represents the quiz."""
     def __init__(self, questions):
         self.questions = questions
         self.score = 0
         self.restart = True
 
-    def run(self):
+    def run(self, interface):
+        """This method runs the quiz."""
         while self.restart:
             self.score = 0
             for question in self.questions:
-                print("\n")
-                print(question.display() + "\nEnter your answer :")
-                answer = input()
+                interface.display_question(question)
+                answer = interface.get_answer(question)
                 if question.check_answer(answer):
                     self.score += 1
-                print("\n")
-            self.display_score()
-            self.ask_restart()
+                print(f"Your current score is: {self.score}")
+            interface.display_score(self.score)
+            self.restart = interface.ask_restart()
 
-    def display_score(self):
-        if self.score == 0:
-            print("Loser ! Your score is ", self.score)
-        elif self.score in [1, 2, 3]:
-            print("You are really not the best... Your score is ", self.score)
-        elif self.score == 4:
-            print("Almost perfect ! Your score is ", self.score)
-        elif self.score == 5:
-            print("Perfect ! Your score is ", self.score)
+class QuizInterface:
+    """This class handles user interactions."""
+    @staticmethod
+    def display_question(question):
+        """This method displays a question."""
+        print(question.display())
 
-    def ask_restart(self):
+    @staticmethod
+    def get_answer(question):
+        """This method gets the user's answer."""
+        while True:
+            answer = input("Enter the number of your answer: ")
+            if answer.isdigit() and 1 <= int(answer) <= len(question.options):
+                return answer
+            else:
+                print("Invalid input. Please enter a valid number.")
+
+    @staticmethod
+    def display_score(score):
+        """This method displays the user's score."""
+        if score == Score.LOSER.value:
+            print("Loser! Your score is ", score)
+        elif score in Score.NOT_BEST.value:
+            print("You are really not the best... Your score is ", score)
+        elif score == Score.ALMOST_PERFECT.value:
+            print("Almost perfect! Your score is ", score)
+        elif score == Score.PERFECT.value:
+            print("Perfect! Your score is ", score)
+
+    @staticmethod
+    def ask_restart():
+        """This method asks the user if they want to restart the quiz."""
         answer = ""
         while answer.lower() not in ["yes", "no"]:
-            print("\nWould you like to try again ? (Yes or No) :")
+            print("\nWould you like to try again? (Yes or No):")
             answer = input()
-        if answer.lower() == "no":
-            self.restart = False
+        return answer.lower() == "yes"
 
 
 question1 = Question("What does the letter H represent in the periodic table?", ["Helium", "Hydrogen", "Hafnium", "Holmium"], "Hydrogen")
@@ -59,4 +90,5 @@ question4 = Question("What does the letter O represent in the periodic table?", 
 question5 = Question("What does the letter K represent in the periodic table?", ["Krypton", "Kryptonite", "Potassium", "Kurium"], "Potassium")
 
 quiz = Quiz([question1, question2, question3, question4, question5])
-quiz.run()
+interface = QuizInterface()
+quiz.run(interface)
